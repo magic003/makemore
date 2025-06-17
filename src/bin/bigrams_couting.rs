2b026372f1c8223c1a52b4e7a58169745223c9f5
Minjie Zha <diego003@gmail.com>
2025-06-16 22:43:13 -0700

    finish bigrams by counting

--- src/bin/bigrams_couting.rs
+++ src/bin/bigrams_couting.rs
@@ -1,5 +1,6 @@
 use std::{char, collections::{HashMap, HashSet}};
-
+use candle_core::{Tensor, DType, Device};
+use rand::{rngs::StdRng, SeedableRng};
 use makemore;
 
 fn main() {
@@ -16,10 +17,61 @@ fn main() {
     println!("Sorted characters: {:?}", chars);
 
     // create char to index and index to char maps
-    let char_to_index: HashMap<char, u32> = chars.iter().enumerate().map(|(i, &c)| (c, i as u32)).collect();
-    let index_to_char: HashMap<u32, char> = chars.iter().enumerate().map(|(i, &c)| (i as u32, c)).collect();
+    let char_to_index: HashMap<char, usize> = chars.iter().enumerate().map(|(i, &c)| (c, i)).collect();
+    let index_to_char: HashMap<usize, char> = chars.iter().enumerate().map(|(i, &c)| (i, c)).collect();
     println!("Character to index map: {:?}", char_to_index);
     println!("Index to character map: {:?}", index_to_char);
     
+    // create a bigram tensor with probabilities
+    let size = chars.len();
+    let mut bigrams_table: Vec<u32> = vec![0; size * size];
+    words.iter().for_each(|word| {
+        let mut prev_char = '.';
+        for c in word.chars() {
+            let prev_index = char_to_index[&prev_char];
+            let curr_index = char_to_index[&c];
+            bigrams_table[prev_index * size + curr_index] += 1;
+            prev_char = c;
+        }
+        // Handle the end of the word
+        let prev_index = char_to_index[&prev_char];
+        let end_index = char_to_index[&'.'];
+        bigrams_table[prev_index * size + end_index] += 1;
+    });
+    let bigrams = Tensor::from_vec(bigrams_table, (size, size), &Device::Cpu)
+        .expect("Failed to create bigram tensor from vector");
+    println!("Bigrams by count: {}", bigrams);
+
+    let bigrams_f32 = (&bigrams + 1.0)
+        .expect("Failed to add one to bigrams tensor")
+        .to_dtype(DType::F32)
+        .expect("Failed to convert bigrams tensor to F32");
+
+    let sums = &bigrams_f32.sum_keepdim(1)
+        .expect("Failed to sum bigrams tensor");
+    let probs = &bigrams_f32.broadcast_div(&sums).expect("Failed to normalize bigrams tensor");
+    println!("Bigrams probability: {}", probs);
+
+    // sampling names
+    let mut rng = StdRng::seed_from_u64(1750132625);
+    let mut output: Vec<String> = vec![];
+    for _ in 0..5 {
+        let mut name = String::new();
+        let mut last_index = 0;
+        loop {
+            let prob = probs.get(last_index)
+                .expect(&format!("Failed to get probabilities for last index: {}", last_index));
+            let next_index = makemore::sampling(&prob, &mut rng)
+                .expect("Failed to sample next index");
+            name.push(index_to_char[&next_index]);
+            last_index = next_index;
+            if last_index == 0 {
+                break;
+            }
+        }
+
+        output.push(name);
+    }
+    println!("Generated names: {:?}", output);
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufReader, BufRead};
+use candle_core::{Tensor, Error};
+use rand::Rng;
 
 /// Reads lines from a file and returns them as a vector of strings.
 pub fn read_lines(filename: &str) -> io::Result<Vec<String>> {
@@ -13,9 +15,22 @@ pub fn read_lines(filename: &str) -> io::Result<Vec<String>> {
     Ok(lines)
 }
 
+/// Samples an index from a tensor based on its probability distribution.
+pub fn sampling(tensor: &Tensor, mut rng: impl Rng) -> Result<usize, Error> {
+    let cumsum: Vec<f32> = tensor.cumsum(0)?.to_vec1()?;
+    let r = rng.random();
+    let index = cumsum
+        .iter()
+        .position(|&x| x >= r)
+        .unwrap_or(cumsum.len() - 1);
+    return Ok(index);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use candle_core::{Tensor, Device};
+    use rand::{rngs::StdRng, SeedableRng};
 
     #[test]
     fn test_read_lines() {
@@ -24,5 +39,25 @@ mod tests {
         assert_eq!(32033, lines.len());
         assert_eq!("emma", lines[0]);
         assert_eq!("olivia", lines[1]);
+    }
+
+    #[test]
+    fn test_sampling() {
+        let tensor = Tensor::from_vec(vec![0.1f32, 0.2, 0.3, 0.4], 4, &Device::Cpu)
+            .expect("Failed to create tensor");
+
+        let mut rng = StdRng::seed_from_u64(1750132625);
+        
+        // 0.01550889
+        let index = sampling(&tensor, &mut rng).expect("Failed to sample index");
+        assert_eq!(0, index);
+
+        // 0.7570026
+        let index = sampling(&tensor, &mut rng).expect("Failed to sample index");
+        assert_eq!(3, index);
+        
+        // 0.62311584
+        let index = sampling(&tensor, &mut rng).expect("Failed to sample index");
+        assert_eq!(3, index);
     }
 }
